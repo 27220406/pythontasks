54528789174d24f8e1798f4b8dcf13e9376fbe43
Simrandeep kaur <jasjas0302@gmail.com>
2025-07-02 10:12:40 +0530

    this list

--- list[].py
+++ list[].py
@@ -2,7 +2,13 @@ my_list = []
 n = int(input("Number of items you want to add? "))
 
 for i in range(n):
-    item = input(f"Enter item {i+1}: ")
+    item = int(input(f"Enter item {i+1}: "))  
     my_list.append(item)
 
 print("Your list is:", my_list)
+maximum = max(my_list)
+minimum = min(my_list)
+print("This is my max:", maximum)
+print("This is my min:", minimum)
+total = sum(my_list)
+print("This is the total sum:", total)
